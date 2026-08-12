d7935f14f77a1266771efd3bf73b906078804110
Sneha234567 <snehaprasad020404@gmail.com>
2026-08-12 10:38:39 +0530

    Update TestFactorial.java

--- TestFactorial.java
+++ TestFactorial.java
@@ -1,1 +1,12 @@
 
+public class TestFactorial {
+    public static void main(String[] args) {
+        int result = Factorial.factorial(5);
+
+        if (result == 120) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+}
